e21ea0ebe048dc99c5ddbf46aaa286f7fcc2b049
Thamatamlahari <lahari12397@gmail.com>
2020-02-12 13:43:15 -0600

    changes made

--- 3reducer.py
+++ 3reducer.py
@@ -3,22 +3,29 @@ r = open("03.txt", "w")
 
 thisKey = ""
 thisValue = 0.0
+count = 0
 
 for line in s:
   data = line.strip().split('\t')
-  store, amount = data
+  paymentType, cost = data
 
-  if store != thisKey:
+  if paymentType != thisKey:
     if thisKey:
       # output the last key value pair result
       r.write(thisKey + '\t' + str(thisValue)+'\n')
 
     # start over when changing keys
-    thisKey = store 
+    thisKey = paymentType
     thisValue = 0.0
   
   # apply the aggregation function
-  thisValue += float(amount)
+    #if(float(cost)>float(thisValue)):
+
+     # count += 1
+     # thisValue = count
+      
+    if float(cost) > float(count):
+      thisValue +=1
 
 # output the final entry when done
 r.write(thisKey + '\t' + str(thisValue)+'\n')
